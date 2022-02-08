815e7d844321c026b2078a48755bf227df3fc77b
guusyy <guus.noij@gmail.com>
2022-02-08 20:08:15 +0100

    added page image conver

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php get_header(); ?>
 
-<div class="container mx-auto my-8">
-
 	<?php if ( have_posts() ) : ?>
 		<?php
 		while ( have_posts() ) :
@@ -13,8 +11,6 @@
 		<?php endwhile; ?>
 
 	<?php endif; ?>
-
-</div>
 
 <?php
 get_footer();
--- template-parts/content.php
+++ template-parts/content.php
@@ -1,42 +1,58 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class( 'mb-12' ); ?>>
+<article <?php post_class( 'mb-12' ); ?>>
 
-  <?php if ( !is_front_page() ) : ?>
-    <header class="entry-header mb-4">
-      <?php the_title( sprintf( '<h1 class="entry-title text-primary text-2xl md:text-3xl font-extrabold leading-tight mb-1"><span class="border-b-2 border-secondary">', esc_url( get_permalink() ) ), '</span></h1>' ); ?>
-    </header>
+  <?php if ( has_post_thumbnail() ) : ?>
+    <div class="h-[380px] md:h-96 overflow-hidden">
+      <?php the_post_thumbnail('full', array('class' => 'w-full h-full object-cover object-top')); ?>
+    </div>
   <?php endif; ?>
+  
+  <div class="container mx-auto my-8">
+    <?php if ( !is_front_page() ) : ?>
+      <header class="entry-header mb-4">
+        <?php the_title( sprintf( '<h1 class="entry-title text-primary text-2xl md:text-3xl font-extrabold leading-tight mb-1"><span class="border-b-2 border-secondary">', esc_url( get_permalink() ) ), '</span></h1>' ); ?>
+      </header>
+    <?php endif; ?>
 
-	<?php if ( is_search() || is_archive() ) : ?>
+    <?php if ( is_search() || is_archive() ) : ?>
 
-		<div class="entry-summary">
-			<?php the_excerpt(); ?>
-		</div>
+      <?php if ( !is_front_page() ) : ?>
+        <header class="entry-header mb-4">
+          <?php the_title( sprintf( '<h1 class="entry-title text-primary text-2xl md:text-3xl font-extrabold leading-tight mb-1"><span class="border-b-2 border-secondary">', esc_url( get_permalink() ) ), '</span></h1>' ); ?>
+        </header>
+      <?php endif; ?>
 
-	<?php else : ?>
+  
+      <div class="entry-summary">
+        <?php the_excerpt(); ?>
+      </div>
+      
+      <?php else : ?>
+  
+      <div class="entry-content">
+        <?php
+        /* translators: %s: Name of current post */
+        the_content(
+          sprintf(
+            __( 'Continue reading %s', 'tailpress' ),
+            the_title( '<span class="screen-reader-text">"', '"</span>', false )
+          )
+        );
+  
+        wp_link_pages(
+          array(
+            'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'tailpress' ) . '</span>',
+            'after'       => '</div>',
+            'link_before' => '<span>',
+            'link_after'  => '</span>',
+            'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'tailpress' ) . ' </span>%',
+            'separator'   => '<span class="screen-reader-text">, </span>',
+          )
+        );
+        ?>
+      </div>
+  
+    <?php endif; ?>
+  </div>
 
-		<div class="entry-content">
-			<?php
-			/* translators: %s: Name of current post */
-			the_content(
-				sprintf(
-					__( 'Continue reading %s', 'tailpress' ),
-					the_title( '<span class="screen-reader-text">"', '"</span>', false )
-				)
-			);
-
-			wp_link_pages(
-				array(
-					'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'tailpress' ) . '</span>',
-					'after'       => '</div>',
-					'link_before' => '<span>',
-					'link_after'  => '</span>',
-					'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'tailpress' ) . ' </span>%',
-					'separator'   => '<span class="screen-reader-text">, </span>',
-				)
-			);
-			?>
-		</div>
-
-	<?php endif; ?>
 
 </article>
