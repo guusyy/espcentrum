fb780e21c6d1f3d9db2758de5b79ba12496ae74a
Guus <guus.noij@gmail.com>
2022-09-30 20:19:45 +0200

    Add favicon package

--- header.php
+++ header.php
@@ -12,6 +12,14 @@
 
   <?php wp_head(); ?>
 
+  <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+  <link rel="manifest" href="/site.webmanifest">
+  <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ff851d">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="theme-color" content="#ffffff">
+
 </head>
 
 <body <?php body_class('bg-white text-gray-900 antialiased text-base mx-auto -z-20'); ?>>
